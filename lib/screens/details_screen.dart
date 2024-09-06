22015a20ca5915ba6ceeb8e985a813de1d7f8927
NoeOnDev <alxg5516@gmail.com>
2024-09-06 10:20:43 -0600

    Refactor constructor in details_screen.dart and home_screen.dart

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
-  const DetailsScreen({Key? key}) : super(key: key);
+  const DetailsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
