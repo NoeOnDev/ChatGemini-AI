dcf430e6fb3b7cfac3eabffddcac50f2165176f1
NoeOnDev <alxg5516@gmail.com>
2024-09-06 14:11:21 -0600

    Refactor constructor in CustomButton widget

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final Color? color;
+
+  const CustomButton({
+    required this.text,
+    required this.onPressed,
+    this.color,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color ?? Theme.of(context).primaryColor,
+      ),
+      child: Text(text),
+    );
+  }
+}
